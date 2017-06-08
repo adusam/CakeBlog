52a90422728765cbe600d49c8254d6405349613d
adusam <popostn@gmail.com>
2017-06-08 11:29:59 +0900

    delete move function

--- blog/src/Controller/CategoriesController.php
+++ blog/src/Controller/CategoriesController.php
@@ -12,7 +12,6 @@ use App\Controller\AppController;
  */
 class CategoriesController extends AppController
 {
-
     /**
      * Index method
      *
@@ -25,38 +24,8 @@ class CategoriesController extends AppController
         ];
         $categories = $this->paginate($this->Categories);
         // $categories = $this->Categories->find()->order(['lft' => 'ASC']);
-
         $this->set(compact('categories'));
         $this->set('_serialize', ['categories']);
-    }
-
-    /**
-     * move function
-     *
-     *
-     */
-    public function moveUp($id = null)
-    {
-        $this->request->allowMethod(['post', 'put']);
-        $category = $this->Categories->get($id);
-        if ($this->Categories->moveUp($category)) {
-            $this->Flash->success('The category has been moved Up.');
-        } else {
-            $this->Flash->error('The category could not be moved up. Please, try again.');
-        }
-        return $this->redirect($this->referer(['action' => 'index']));
-    }
-
-    public function moveDown($id = null)
-    {
-        $this->request->allowMethod(['post', 'put']);
-        $category = $this->Categories->get($id);
-        if ($this->Categories->moveDown($category)) {
-            $this->Flash->success('The category has been moved down.');
-        } else {
-            $this->Flash->error('The category could not be moved down. Please, try again.');
-        }
-        return $this->redirect($this->referer(['action' => 'index']));
     }
 
     /**
