69a1aba2cb439637171420c029384e4a8e967c57
mklpij21985 <mklpij21985@yahoo.co.jp>
2017-06-28 16:50:12 +0900

    画像アップロード
    表示できなかった部分の修正

--- blog/src/Controller/ManageController.php
+++ blog/src/Controller/ManageController.php
@@ -8,27 +8,27 @@ use Cake\Filesystem\File;
 use RuntimeException;
 
 /**
- * Manage Controller
- *
- * @property \App\Model\Table\ArticlesTable $Manage
- *
- * @method \App\Model\Entity\Articles[] paginate($object = null, array $settings = [])
- */
+* Manage Controller
+*
+* @property \App\Model\Table\ArticlesTable $Manage
+*
+* @method \App\Model\Entity\Articles[] paginate($object = null, array $settings = [])
+*/
 
 class ManageController extends AppController
 {
 
     public function initialize()
-   {
-       parent::initialize();
-       $this->Articles = TableRegistry::get('Articles');
-       $this->Pictures = TableRegistry::get('Pictures');
-   }
+    {
+        parent::initialize();
+        $this->Articles = TableRegistry::get('Articles');
+        $this->Pictures = TableRegistry::get('Pictures');
+    }
     /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null
-     */
+    * Index method
+    *
+    * @return \Cake\Http\Response|null
+    */
     public function index()
     {
         $this->paginate = [
@@ -45,12 +45,12 @@ class ManageController extends AppController
     }
 
     /**
-     * Edit method
-     *
-     * @param string|null $id Article id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
+    * Edit method
+    *
+    * @param string|null $id Article id.
+    * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+    * @throws \Cake\Network\Exception\NotFoundException When record not found.
+    */
     public function edit($id = null)
     {
         if ($id === null) {
@@ -82,35 +82,35 @@ class ManageController extends AppController
 
                 $filename = date('ymdHis_').$this->request->data['picture_id']['name'];
                 // $fileInfo = new File("/xampp/htdocs/CakeBlog/blog/webroot/uploads/pictures/");
+        
 
+                if($img_extension = array_search($mime_type, $extension_array) || $tmp === ""){
+                    if(is_uploaded_file($tmp)) {
+                        $dir = "/xampp/htdocs/CakeBlog/blog/webroot/uploads/pictures";
+                        move_uploaded_file($tmp, $dir . DS . $filename);
 
+                        $picture = $this->Pictures->patchEntity($picture, ['data' => $filename]);
+                        if ($this->Pictures->save($picture)) {
+                            echo "saved\n";
+                        }
+                        else {
+                            $this->Flash->error(__('画像の保存ができませんでした。'));
+                        }
+                    }
+                }
+                else{
+                    $this->Flash->error(__('このファイルはアップロードできません。'));
+                    return;
+                }
 
             }
             $article['picture_id'] = $picture['id'];
 
-                if($img_extension = array_search($mime_type, $extension_array) || $tmp === "")
-                    {
-                        if(is_uploaded_file($tmp)) {
-                            $dir = "/xampp/htdocs/CakeBlog/blog/webroot/uploads/pictures";
-                            move_uploaded_file($tmp, $dir . DS . $filename);
-
-                            $picture = $this->Pictures->patchEntity($picture, ['data' => $filename]);
-                            if ($this->Pictures->save($picture)) {
-                                echo "saved\n";
-                            }
-                            else {
-                                $this->Flash->error(__('画像の保存ができませんでした。'));
-                            }
-                        }
-                        if ($this->Articles->save($article)) {
-                        $this->Flash->success(__('記事が投稿されました。'));
-                        return $this->redirect(['action' => 'index']);
-                    }
-                        $this->Flash->error(__('記事の投稿ができませんでした。再度、投稿をお願いします。'));
-                    }
-                    else{
-                    $this->Flash->error(__('このファイルはアップロードできません。'));
-                    }
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('記事が投稿されました。'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('記事の投稿ができませんでした。再度、投稿をお願いします。'));
         }
 
         $this->set(compact('article'));
@@ -121,12 +121,12 @@ class ManageController extends AppController
     }
 
     /**
-     * Delete method
-     *
-     * @param string|null $id Article id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
+    * Delete method
+    *
+    * @param string|null $id Article id.
+    * @return \Cake\Http\Response|null Redirects to index.
+    * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+    */
     public function delete($id = null)
     {
         $this->request->allowMethod(['post', 'delete']);
